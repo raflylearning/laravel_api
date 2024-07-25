0422465d0e9ec5eba3d27dbabfc56e5ef49861b3
raflylearning <raflyayanze24@gmail.com>
2024-07-25 13:56:19 +0700

    first_API

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Kategoricontroller;
+use App\Http\Controllers\Genrecontroller;
+use App\Http\Controllers\AktorController;
+use App\Http\Controllers\Filmcontroller;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware((['auth:sanctum']))->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::resource('kategori', Kategoricontroller::class);
+    Route::resource('genre', Genrecontroller::class);
+    Route::resource('aktor', AktorController::class);
+    Route::resource('film', Filmcontroller::class);
+});
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
