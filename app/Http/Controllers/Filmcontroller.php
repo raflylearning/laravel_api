b7ce1a1c006bac08fb7034879a5dda0825d2148c
raflylearning <raflyayanze24@gmail.com>
2024-07-26 14:35:42 +0700

    first_API

--- app/Http/Controllers/Filmcontroller.php
+++ app/Http/Controllers/Filmcontroller.php
@@ -1,8 +1,6 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Film;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -12,25 +10,25 @@ class FilmController extends Controller
 {
     public function index()
     {
-        $films = Film::with(['genre', 'aktor'])->get();
+        $film = Film::with(['genre', 'actor'])->get();
         return response()->json([
             'success' => true,
             'message' => 'Data Film',
-            'data' => $films,
+            'data' => $film,
         ], 200);
     }
 
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'judul' => 'required|string|unique:films',
+            'judul' => 'required|string|unique:film',
             'slug' => 'required|string',
             'deskripsi' => 'required|string',
             'foto' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'url_video' => 'required|string',
-            'id_kategori' => 'required|exists:kategoris,id',
+            'url_vidio' => 'required|string',
+            'id_kategoris' => 'required|exists:kategoris,id',
             'genre' => 'required|array',
-            'aktor' => 'required|array',
+            'actor' => 'required|array',
         ]);
 
         if ($validator->fails()) {
@@ -46,15 +44,15 @@ class FilmController extends Controller
 
             $film = Film::create([
                 'judul' => $request->judul,
-                'slug' => $request->slug,
                 'deskripsi' => $request->deskripsi,
                 'foto' => $path,
-                'url_video' => $request->url_video,
-                'id_kategori' => $request->id_kategori,
+                'url_vidio' => $request->url_vidio,
+                'id_kategoris' => $request->id_kategoris,
+                'slug' => $request->slug,
             ]);
 
             $film->genre()->sync($request->genre);
-            $film->aktor()->sync($request->aktor);
+            $film->actor()->sync($request->actor);
 
             return response()->json([
                 'success' => true,
@@ -73,7 +71,7 @@ class FilmController extends Controller
     public function show($id)
     {
         try {
-            $film = Film::with(['genre', 'aktor'])->findOrFail($id);
+            $film = Film::with(['genre', 'actor'])->findOrFail($id);
             return response()->json([
                 'success' => true,
                 'message' => 'Detail Film',
@@ -83,6 +81,7 @@ class FilmController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Data tidak ditemukan',
+                'errors' => $e->getMessage(),
             ], 404);
         }
     }
@@ -92,14 +91,14 @@ class FilmController extends Controller
         $film = Film::findOrFail($id);
 
         $validator = Validator::make($request->all(), [
-            'judul' => 'required|string|unique:films,judul,' . $id,
-            'slug' => 'required|string',
+            'judul' => 'required|string|unique:film,judul,' . $id,
+            'slug' => 'required|string,' . $id,
             'deskripsi' => 'required|string',
             'foto' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'url_video' => 'required|string',
-            'id_kategori' => 'required|exists:kategoris,id',
+            'url_vidio' => 'required|string',
+            'id_kategoris' => 'required|exists:kategoris,id',
             'genre' => 'required|array',
-            'aktor' => 'required|array',
+            'actor' => 'required|array',
         ]);
 
         if ($validator->fails()) {
@@ -119,14 +118,14 @@ class FilmController extends Controller
                 $film->foto = $path;
             }
 
-            $film->update($request->only(['judul', 'deskripsi', 'url_video', 'id_kategori']));
+            $film->update($request->only(['judul', 'deskripsi', 'url_vidio', 'id_ss']));
 
             if ($request->has('genre')) {
                 $film->genre()->sync($request->genre);
             }
 
-            if ($request->has('aktor')) {
-                $film->aktor()->sync($request->aktor);
+            if ($request->has('actor')) {
+                $film->actor()->sync($request->actor);
             }
 
             return response()->json([
@@ -142,7 +141,6 @@ class FilmController extends Controller
             ], 500);
         }
     }
-
     public function destroy($id)
     {
         try {
@@ -150,20 +148,20 @@ class FilmController extends Controller
 
             // Delete photo
             Storage::delete($film->foto);
-
+            $film->genre()->detech();
+            $film->actor()->detech();
             $film->delete();
-            $film->aktor()->detach();
-            $film->genre()->detach();
 
             return response()->json([
                 'success' => true,
                 'message' => 'Data deleted successfully',
                 'data' => null,
-            ], 200);
+            ], 204);
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Data not found',
+                'errors' => $e->getMessage(),
             ], 404);
         }
     }
